934c23ff1dde4c7b108cf2e4033adb00d0250d27
sion <parksion9262@gmail.com>
2020-04-08 17:48:10 +0900

    4주차

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,36 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "201621196박시온 onCreate() called!");
+        Log.e(TAG, "201621196박시온 onCreate() called!");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "201621196박시온 onStart() called!");
+        Log.e(TAG, "201621196박시온 onStart() called!");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "201621196박시온 onRestart() called!");
+        Log.e(TAG, "201621196박시온 onRestart() called!");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "201621196박시온 onStrop() called!");
+        Log.e(TAG, "201621196박시온 onStop() called!");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "201621196박시온 onPause() called!");
+        Log.e(TAG, "201621196박시온 onPause() called!");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "201621196박시온 onDestroy() called!");
+        Log.e(TAG, "201621196박시온 onDestroy() called!");
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Sandbox.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import java.util.*;
+public class Sandbox {
+    private List<String> mGreetings = new ArrayList<String>();
+
+    public List<String> getmGreetings() {
+        return mGreetings;
+    }
+
+    public void setmGreetings(List<String> mGreetings) {
+        this.mGreetings = mGreetings;
+    }
+
+    public Sandbox() {
+    }
+}
